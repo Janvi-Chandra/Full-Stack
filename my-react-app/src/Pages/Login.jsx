4cf4b9dc334105da52b4b62cb37f8bed208031b6
falakahuja <falakahuja0101@gmail.com>
2024-07-26 18:46:50 +0530

    Login page added

---
+++ my-react-app/src/Pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginComp from '../Components/LoginComp';
+import '../Components/LoginComp.css';
+
+const Login = () => {
+  return (
+    <div className="contact-container">
+      <LoginComp />
+    </div>
+  );
+};
+
+export default Login;
